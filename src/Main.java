497deea8cc5295ab2fa67fd22d4dbcebb16dff68
GodotColombia <jrocano@elpuig.xeill.net>
2026-09-15 20:47:46 +0200

    Exercici Pràctic: Navegació per Menú en Consola TERMINADO

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+
+public class  Main {
+    Scanner sc = new Scanner(System.in);
+    ArrayList<String> usuarios = new ArrayList<>();
+
+    public void main(String[] args) {
+        while (true) {
+            System.out.println("---------------------------------");
+            System.out.println("Elige una opción");
+            System.out.println("0 - Sortir");
+            System.out.println("1 - Abrir gestión de usuarios");
+            System.out.println("2 - Veure estat del sistema");
+            System.out.println("---------------------------------");
+            int opcion = sc.nextInt();
+            switch (opcion) {
+                case 0:
+                    return;
+                case 1:
+                    obrirSubmenuUsuaris(usuarios);
+                    break;
+                case 2:
+                    System.out.println("Estado: Corrupto");
+                    break;
+                default:
+                    System.out.println("Opción no valida yaguete");
+            }
+        }
     }
+
+    public void obrirSubmenuUsuaris(ArrayList usuarios) {
+        while (true) {
+            System.out.println("*-------------------------------*");
+            System.out.println("0 - Volver");
+            System.out.println("1 - Listar usuarios");
+            System.out.println("2 - Añadir usuario");
+            System.out.println("*-------------------------------*");
+
+            int opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                   System.out.println(usuarios);
+                   break;
+                case 2:
+                    System.out.println("Escriba el nombre del usuario");
+                    String usuari = sc.next();
+                    usuarios.add(usuari);
+                    System.out.println("Usuario creado correctamente");
+                    break;
+                case 0:
+                    return;
+            }
+        }
+    }
+
+
 }
